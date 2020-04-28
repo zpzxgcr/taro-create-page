0f79f4da98e3ffc254e5cef98819f134e6b21f3e
wly <zpzxgcr@163.com>
2020-04-28 15:49:17 +0800

    doc: 更新文档说明

--- README.md
+++ README.md
@@ -4,18 +4,45 @@
 * 直接taro插件配置
 
 用法
+```shell
+> npm install -D  taro-create-page
+```
+> 默认生成 是class 组件模板
 ```javascript
 //在项目 config/index.js配置
 const AutoCreatePage = require('taro-create-page')
 
 {
 ...
+
   plugins: [
     new AutoCreatePage()
   ]
+
 ...
 }
 ```
+
+> 如果需要初始化生成hooks 模板 请在你的项目路径配置后面加上注释
+```tsx
+...
+
+ config: Config = {
+    pages: [
+      'pages/index/index', /* 首页 */
+      'pages/exchange/index', /* 我的变化 */
+      'pages/login/index', /* 登录 ishooks*/
+      'pages/my-reward/index',
+      'pages/rank/index', /* 排行榜 */
+      'pages/result/index', /* 题目分数 */
+      'pages/topic/index' /* 题目首页 */
+
+    ]
+
+  ...
+```
+**上面 路径后面的注释 包含中文便是新建模板的navigationBarTitleText, 否则默认路径**
+
 适用于项目初始化 设计好目录路径结构 自动 生成目录路径
 原有项目开发的时候 不需要新建文件夹 模板 然后 再去添加路径
 自动完成
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-
+/* eslint-disable */
 const babylon = require('@babel/parser')
-const traverse = require("@babel/traverse").default;
-const t = require("@babel/types");
+const traverse = require('@babel/traverse').default
+const t = require('@babel/types')
 const path = require('path')
 const chalk = require('chalk')
-const ora = require('ora')
 const template = require('art-template')
 const fs = require('fs-extra')
-
 const classTemplat = require.resolve('./template/class.art')
 const hooksTemplat = require.resolve('./template/hooks.art')
 const log = console.log
 
 const babylonConfig = {
-   sourceType: 'module',
-    plugins: [
+  sourceType: 'module',
+  plugins: [
     'typescript',
     'classProperties',
     'jsx',
@@ -26,100 +24,93 @@ const babylonConfig = {
     'dynamicImport'
   ]
 
-  }
+}
 
 /**
  * 首字母大写
  * @param {string} str
  */
 function firetUpperCase (str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 class AutoCreatePage {
   constructor () {
-    this.source = 'src';
-    this.pages = [];
-    this.parseCode = this.parseCode.bind(this);
-    this.writeTemplate = this.writeTemplate.bind(this);
-    const jsType = ['app.tsx', 'app.js', 'app.ts'];
-    const cssType = ['app.css', 'app.scss', 'app.less', 'app.styl'];
-    const rootJsType = jsType.filter(item => fs.pathExistsSync(path.join(this.source, item)));
-    const rootCssType = cssType.filter(item => fs.pathExistsSync(path.join(this.source, item)));
-    this.rootJsType = rootJsType[0] || 'app.jsx';
-    this.rootCssType = rootCssType[0] || 'app.css';
-    this.parseCode();
-  }
-  async existsRootFileType (type) {
-    console.log(fs.pathExistsSync(path.join(this.source, type)), '路径存在');
-
-    if (fs.pathExistsSync(path.join(this.source, type))) return type;
-    else return null;
+    this.source = 'src'
+    this.pages = []
+    this.parseCode = this.parseCode.bind(this)
+    this.writeTemplate = this.writeTemplate.bind(this)
+    const jsType = ['app.tsx', 'app.js', 'app.ts']
+    const cssType = ['app.css', 'app.scss', 'app.less', 'app.styl']
+    const rootJsType = jsType.filter(item => fs.pathExistsSync(path.join(this.source, item)))
+    const rootCssType = cssType.filter(item => fs.pathExistsSync(path.join(this.source, item)))
+    this.rootJsType = rootJsType[0] || 'app.jsx'
+    this.rootCssType = rootCssType[0] || 'app.css'
+    const _this = this
+    fs.watchFile(path.join(this.source, this.rootJsType), (path) => {
+      _this.parseCode()
+    })
   }
 
   apply (builder) {
     builder.hooks.beforeBuild.tap('BuildPlugin', (config) => {
-      this.source = config.sourceRoot;
-    })
-
-    builder.hooks.afterBuild.tap('BuildPlugin', (stats) => {
-      this.parseCode();
+      this.source = config.sourceRoot
     })
   }
 
   parseCode () {
-    const _this = this;
-    const code = fs.readFileSync(path.join(this.source,this.rootJsType), "utf8");
-    let ast = babylon.parse(code, babylonConfig);
-    let pages = [];
+    const _this = this
+    const code = fs.readFileSync(path.join(this.source, this.rootJsType), 'utf8')
+    let ast = babylon.parse(code, babylonConfig)
+    let pages = []
     traverse(ast, {
-       ObjectProperty (astPath) {
-          const node = astPath.node
-          const key = node.key
-          const value = node.value
+      ObjectProperty(astPath) {
+        const node = astPath.node
+        const key = node.key
+        const value = node.value
 
-          if (key.value === 'pages' && t.isArrayExpression(value)) {
-            // 分包
-            let root = ''
-            const rootNode = astPath.parent.properties.find(v => {
-              return v.key.value === 'root'
-            })
-            root = rootNode ? rootNode.value.value : ''
-            value.elements.forEach(v => {
-              if (t.isStringLiteral(v)) {
-                const pagePath = `${root}/${v.value}`.replace(/\/{2,}/g, '/').replace(/^\//, '');
-                const fielName = pagePath.slice(pagePath.lastIndexOf('/') + 1, pagePath.length);
-                const name = firetUpperCase(fielName);
-                const cssType = `${fielName}${path.extname(_this.rootCssType)}`;
-                let title =  fielName;
-                let isHooks = false;
-                if (v.trailingComments) {
-                  const { value } = v.trailingComments[0];
-                  title = value.match(/[\u4e00-\u9fa5]+/g)[0];
-                  isHooks = /ishooks/i.test(value);
-                }
-                _this.writeTemplate({
-                  title, isHooks, cssType, name, fielName, pagePath
-                })
+        if (key.name === 'pages' && t.isArrayExpression(value)) {
+          // 分包
+          let root = ''
+          const rootNode = astPath.parent.properties.find(v => {
+            return v.key.value === 'root'
+          })
+          root = rootNode ? rootNode.value.value : ''
+
+          value.elements.forEach(v => {
+            if (t.isStringLiteral(v)) {
+              const pagePath = `${root}/${v.value}`.replace(/\/{2,}/g, '/').replace(/^\//, '')
+              const fielName = pagePath.slice(pagePath.lastIndexOf('/') + 1, pagePath.length)
+              const name = firetUpperCase(fielName)
+              const cssType = `${fielName}${path.extname(_this.rootCssType)}`
+              let title = fielName
+              let isHooks = false
+              if (v.trailingComments) {
+                const { value } = v.trailingComments[0]
+                title = value.match(/[\u4e00-\u9fa5]+/g)[0]
+                isHooks = /ishooks/i.test(value)
               }
-            });
-
-          }
+              _this.writeTemplate({
+              title, isHooks, cssType, name, fielName, pagePath})
+            }
+          })
         }
-    }) ;
+      }
+    })
   }
 
   writeTemplate ({title, isHooks, cssType, name, fielName, pagePath}) {
-    const exists = fs.pathExistsSync(path.join(this.source, `${pagePath}${path.extname(this.rootJsType)}`));
+    const exists = fs.pathExistsSync(path.join(this.source, `${pagePath}${path.extname(this.rootJsType)}`))
+
     if (!exists) {
       const html = template(isHooks ? hooksTemplat : classTemplat, {
         title,
         name,
         type: cssType
-      });
-      fs.outputFile(path.join(this.source, `${pagePath}${path.extname(this.rootJsType)}`), html);
-      fs.ensureFile(path.join(this.source, `${pagePath}${path.extname(this.rootCssType)}`));
-      log(chalk.green(`编译`),` 发现路径 ${pagePath}`);
+      })
+      fs.outputFile(path.join(this.source, `${pagePath}${path.extname(this.rootJsType)}`), html)
+      fs.ensureFile(path.join(this.source, `${pagePath}${path.extname(this.rootCssType)}`))
+      log(chalk.green(`编译`), ` 发现路径 ${pagePath}`)
     }
   }
 }
